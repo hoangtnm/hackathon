ac51083bf4be3bddfe989d7f884738dadb387fdb
hoangtnm <minhhoangtrannhat@gmail.com>
2019-11-21 15:39:43 +0700

    update split_dataset

--- utils.py
+++ utils.py
@@ -69,10 +69,11 @@ def split_image_folder(in_dir, out_dir, train_size=0.8):
 
     if os.path.exists(out_dir):
         shutil.rmtree(out_dir)
-    else:
-        os.makedirs(out_dir)
 
     for class_name in class_names:
+        os.makedirs(os.path.join(out_dir, 'train', class_name))
+        os.makedirs(os.path.join(out_dir, 'val', class_name))
+
         img_list = os.listdir(os.path.join(in_dir, class_name))
         random.shuffle(img_list)
         num_images = len(img_list)
@@ -82,10 +83,10 @@ def split_image_folder(in_dir, out_dir, train_size=0.8):
         val_images = img_list[num_train:]
         for image in train_images:
             shutil.copy2(os.path.join(in_dir, class_name, image),
-                         os.path.join(out_dir, 'train', class_name, image))
+                         os.path.join(out_dir, 'train', class_name))
         for image in val_images:
             shutil.copy2(os.path.join(in_dir, class_name, image),
-                         os.path.join(out_dir, 'val', class_name, image))
+                         os.path.join(out_dir, 'val', class_name))
 
 
 def get_device():
