6c002d3a65055410c7a6283a6010da05db54b805
hoangtnm <minhhoangtrannhat@gmail.com>
2019-11-21 18:18:46 +0700

    add visualization

--- utils.py
+++ utils.py
@@ -142,6 +142,49 @@ def preprocess_image(image_np, mode='train'):
     return input_tensor
 
 
+def imshow(inp, title=None):
+    """Imshow for Tensor."""
+    inp = inp.numpy().transpose((1, 2, 0))
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    inp = std * inp + mean
+    inp = np.clip(inp, 0, 1)
+    plt.imshow(inp)
+    if title is not None:
+        plt.title(title)
+    plt.pause(0.001)  # pause a bit so that plots are updated
+
+
+def visualize_model(model, dataloaders, num_images=6):
+    class_names = dataloaders['val'].dataset.classes
+    device = get_device()
+    was_training = model.training
+    model.eval()
+
+    images_so_far = 0
+    fig = plt.figure()
+
+    with torch.no_grad():
+        for i, (inputs, labels) in enumerate(dataloaders['val']):
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            outputs = model(inputs)
+            _, predictions = torch.max(outputs, 1)
+
+            for j in range(inputs.size()[0]):
+                images_so_far += 1
+                ax = plt.subplot(num_images // 2, 2, images_so_far)
+                ax.axis('off')
+                ax.set_title('predicted: {}'.format(class_names[predictions[j]]))
+                imshow(inputs.cpu().data[j])
+
+                if images_so_far == num_images:
+                    model.train(mode=was_training)
+                    return
+        model.train(mode=was_training)
+
+
 def get_prediction_class(prediction, idx_to_class):
     """Returns class of prediction.
     Args:
