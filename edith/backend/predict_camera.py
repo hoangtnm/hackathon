4d573048fa4cc4b7d331bc22e67a8882771c14c6
hoangtnm <minhhoangtrannhat@gmail.com>
2019-12-15 18:26:16 +0700

    update predict_camera.py

--- edith/backend/predict_camera.py
+++ edith/backend/predict_camera.py
@@ -1,44 +1,25 @@
 import os
 
 import cv2
-import torch
-
 from config import CHECKPOINT_PATH
-from config import DATASET_PATH
+from config import CLASSES
 from utils import get_device
-from utils import get_metadata
 from utils import get_net
-from utils import get_prediction_class
-from utils import preprocess_image
+from utils import get_result
 
 if __name__ == '__main__':
     device = get_device()
+    num_classes = len(CLASSES)
 
-    # Training dataset metadata
-    _, class_names, class_to_idx = get_metadata(
-        os.path.join(DATASET_PATH, 'train')
-    )
-    num_classes = len(class_names)
-    idx_to_class = {value: key for key, value in class_to_idx.items()}
+    net = get_net(model_name='mobilenet_v2', mode='inference', device=device,
+                  pretrained=False, num_classes=num_classes, checkpoint_path=CHECKPOINT_PATH)
 
-    # Net initialization
-    net = get_net(classes=num_classes)
-    checkpoint_dict = torch.load(CHECKPOINT_PATH, map_location=device)
-    net.load_state_dict(checkpoint_dict['model_state_dict'])
-    net.eval()
-    net.to(device)
-
-    cap = cv2.VideoCapture(0)
+    # cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(os.path.join('videos', 'video_03.mp4'))
 
     while True:
         _, frame = cap.read()
-        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-        # Prediction
-        frame_tensor = preprocess_image(frame_rgb, mode='inference')
-        frame_tensor = frame_tensor.to(device)
-        prediction = net(frame_tensor)
-        result = get_prediction_class(prediction, idx_to_class)
+        result = get_result(frame, CLASSES, net, device)
         print(f'Result: {result}')
 
         # Display the resulting frame
