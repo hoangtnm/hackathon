4b0d579a85a646c8f6d400ef1d7bc5a9ae78e687
hoangtnm <minhhoangtrannhat@gmail.com>
2019-12-15 23:29:13 +0700

    update predict_camera.py

--- edith/backend/predict_camera.py
+++ edith/backend/predict_camera.py
@@ -11,11 +11,11 @@ if __name__ == '__main__':
     device = get_device()
     num_classes = len(CLASSES)
 
-    net = get_net(model_name='mobilenet_v2', mode='inference', device=device,
-                  pretrained=False, num_classes=num_classes, checkpoint_path=CHECKPOINT_PATH)
+    net = get_net('mobilenet_v2', 'val', device, num_classes=num_classes,
+                  checkpoint_path=CHECKPOINT_PATH)
 
     # cap = cv2.VideoCapture(0)
-    cap = cv2.VideoCapture(os.path.join('videos', 'video_03.mp4'))
+    cap = cv2.VideoCapture(os.path.join('videos', 'video_01.mp4'))
 
     while True:
         _, frame = cap.read()
